5250839c171bcc35e7d196625b3c4a2f071d069f
h-havnursangappa <h.havnursangappa@gmail.com>
2021-02-07 21:44:26 +0200

    Implemented the basic user interface components

--- Password-manager/user_interface.py
+++ Password-manager/user_interface.py
@@ -1,8 +1,270 @@
-from sql_setup import Database
+import tkinter as tk
 import sys
+from tkinter import simpledialog
+from tkinter import messagebox
+from sql_setup import Database
 
 db = Database()  # Instantiate the database object
 
+class UserInterface:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Hello User")
+
+        top_frame = tk.Frame(self.root)
+        top_frame.pack(side='top')
+
+        bottom_frame = tk.Frame(self.root)
+        bottom_frame.pack(side='top')
+
+        welcome_label = tk.Label(top_frame, text="Welcome to your Password manager")
+        welcome_label.pack(side='top', padx=5, pady=5)
+
+        login_btn = tk.Button(bottom_frame, text="Login", width=10, command=self.login_window)
+        login_btn.pack(side='left', padx=5, pady=5)
+
+        create_btn = tk.Button(bottom_frame, text="Create Vault", width=10, command=self.create_password_window)
+        create_btn.pack(side='left', padx=5, pady=5)
+
+        self.root.mainloop()
+
+
+    def login_window(self):
+        self.login_win = tk.Toplevel()
+        self.login_win.title("Vault Login")
+        self.login_win.grab_set()
+
+        frame = tk.Frame(self.login_win)
+        frame.pack(side='top')
+
+        prompt_label = tk.Label(frame, text="Enter master password to login")
+        prompt_label.pack(side='top', padx=5, pady=5)
+
+        self.pass_field = tk.Entry(frame, show='*', width=15)
+        self.pass_field.pack(side='top', padx=5, pady=5)
+
+        login_btn = tk.Button(frame, text="Login", width=10, command=self.login_password_vault)
+        login_btn.pack(side='top', padx=5, pady=5)
+
+
+    def login_password_vault(self):
+        m_data = db.is_master_data()
+        if m_data:
+            m_pwd = db.return_master_password()
+            pass_key = str(self.pass_field.get())
+            attempt = 0
+            while pass_key != m_pwd:
+                if attempt <= 3:
+                    pass_key = simpledialog.askstring(title="Login Failed", prompt=f"Wrong password. You have {3 - attempt} attempts left. Please try again: ")
+                    attempt += 1
+                    self.login_win.wait_window()
+                else:
+                    print("You have exhausted your attempts. Try again after 24 hrs")
+                    sys.exit()
+
+            messagebox.showinfo("Login Successfull", "You have successfully logged in")
+            # print('\n')
+            self.password_table()
+
+        else:
+            messagebox.showwarning("Login Failed", "You have not yet created a vault. Create one by entering the master password")
+            self.login_win.destroy()
+
+
+    def create_password_window(self):
+        self.pass_win = tk.Toplevel()
+        self.pass_win.title("Create Vault")
+        self.pass_win.grab_set()
+
+        frame = tk.Frame(self.pass_win)
+        frame.pack(side='top')
+
+        prompt_label = tk.Label(frame, text="Enter master password")
+        prompt_label.pack(side='top', padx=5, pady=5)
+
+        self.pass_field = tk.Entry(frame, show='*', width=15)
+        self.pass_field.pack(side='top', padx=5, pady=5)
+
+        create_btn = tk.Button(frame, text="Create", width=10, command=self.create_password_vault)
+        create_btn.pack(side='top', padx=5, pady=5)
+
+
+    def create_password_vault(self):
+        db.remove_all_values()
+        db.create_master_password_table(self.pass_field.get())
+        messagebox.showinfo("Vault creation successfull", "A Vault has been successfully created for you. Now you can do the following: ")
+        self.pass_win.destroy()
+        self.password_table()
+
+
+    def password_table(self):
+        self.root.withdraw()
+        self.login_win.destroy()
+
+        self.pass_table = tk.Toplevel()
+        self.pass_table.title("Password Manager")
+
+        top_frame = tk.Frame(self.pass_table)
+        top_frame.pack(side='top')
+
+        bottom_frame = tk.Frame(self.pass_table)
+        bottom_frame.pack(side='top')
+
+        bottom_left_frame = tk.Frame(self.pass_table)
+        bottom_left_frame.pack(side='left')
+
+        bottom_right_frame = tk.Frame(self.pass_table)
+        bottom_right_frame.pack(side='left')
+
+        # Create the table here
+
+        add_entry_btn = tk.Button(bottom_left_frame, text="Add credentials", width=15, command=self.add_credentials_window)
+        add_entry_btn.pack(side='top', padx=5, pady=5)
+
+        delete_entry_btn = tk.Button(bottom_left_frame, text="Delete a credential", width=15, command=self.delete_credentials_window)
+        delete_entry_btn.pack(side='top', padx=5, pady=5)
+
+        delete_all_btn = tk.Button(bottom_left_frame, text="Delete all credentials", width=15, command=self.delete_all_credentials)
+        delete_all_btn.pack(side='top', padx=5, pady=5)
+
+        change_mas_pwd_btn = tk.Button(bottom_right_frame, text="Change master password", width=15, command=self.change_master_password_window)
+        change_mas_pwd_btn.pack(side='top', padx=5, pady=5)
+
+        logout_btn = tk.Button(bottom_right_frame, text="Logout", width=15, command=self.logout)
+        logout_btn.pack(side='top', padx=5, pady=5)
+
+
+    def add_credentials_window(self):
+        cred_win = tk.Toplevel()
+        cred_win.title = 'Add a new entry'
+
+        web_label = tk.Label(cred_win, text="Enter the website")
+        web_label.grid(row=0, column=0, padx=5, pady=5, sticky='e')
+
+        self.web_field = tk.Entry(cred_win)
+        self.web_field.grid(row=0, column=1, padx=5, pady=5, sticky='ew')
+
+        user_label = tk.Label(cred_win, text="Enter the username")
+        user_label.grid(row=1, column=0, padx=5, pady=5, sticky='e')
+
+        self.user_field = tk.Entry(cred_win)
+        self.user_field.grid(row=1, column=1, padx=5, pady=5, sticky='ew')
+
+        pass_label = tk.Label(cred_win, text="Enter the passsword")
+        pass_label.grid(row=2, column=0, padx=5, pady=5, sticky='e')
+
+        self.pass_field = tk.Entry(cred_win)
+        self.pass_field.grid(row=2, column=1, padx=5, pady=5, sticky='ew')
+
+        add_btn = tk.Button(cred_win, text='Add', comamnd=self.add_credentials)
+        add_btn.grid(row=3, column=0, padx=5, pady=5, sticky='ew')
+
+
+    def add_credentials(self):
+        web = str(self.web_field.get())
+        username = str(self.user_field.get())
+        password = str(self.pass_field.get())
+
+        table_exists = db.is_password_table()
+        if table_exists:
+            sl_no = db.return_serial_number()
+        else:
+            sl_no = 1
+        db.create_pwd_table(sl_no, web, username, password)
+        messagebox.showinfo("The password has been successfully added to the vault")
+        self.pass_table.wait_window()
+
+
+    def delete_credentials_window(self):
+        # delete_cred_win = tk.Toplevel()
+        # delete_cred_win.title = "Delete existing credential"
+        ans = messagebox.askyesnocancel("Delete existing credential", "Are you sure you want to delete these credentials from the vault?")
+        if ans == 'yes':
+            db.remove_values()
+        else:
+            self.pass_table.wait_window()
+
+
+    def delete_all_credentials(self):
+        ans = messagebox.askyesnocancel("Delete all credentials", "Are you sure you want to delete all credentials and clear the vault?")
+        if ans == 'yes':
+            db.remove_all_values()
+        else:
+            self.pass_table.wait_window()
+
+
+    def change_master_password_window(self):
+        self.chng_master_pwd_win = tk.Toplevel()
+        self.chng_master_pwd_win.title = "Change the master password"
+
+        curr_pass_label = tk.Label("Enter the current master password")
+        curr_pass_label.grid(row=0, column=0, padx=5, pady=5, sticky='e')
+
+        self.curr_pass_field = tk.Entry(self.chng_master_pwd_win)
+        self.curr_pass_field.grid(row=0, column=1, padx=5, pady=5, sticky='ew')
+
+        new_pass_label = tk.Label("Enter the new master password")
+        new_pass_label.grid(row=1, column=0, padx=5, pady=5, sticky='e')
+
+        self.new_pass_field = tk.Entry(self.chng_master_pwd_win)
+        self.new_pass_field.grid(row=1, column=1, padx=5, pady=5, sticky='ew')
+
+        conf_pass_label = tk.Label("Confirm the new master password")
+        conf_pass_label.grid(row=2, column=0, padx=5, pady=5, sticky='e')
+
+        self.conf_pass_field = tk.Entry(self.chng_master_pwd_win)
+        self.conf_pass_field.grid(row=2, column=1, padx=5, pady=5, sticky='ew')
+
+        change_pass_btn = tk.Button(self.chng_master_pwd_win, text='Change Master Password', command=self.change_master_password)
+        change_pass_btn.grid(row=3, column=0, padx=5, pady=5, sticky='ew')
+
+
+    def change_master_password(self):
+        curr_m_pwd = str(self.curr_pass_field.get())
+        new_m_pwd = str(self.new_pass_field.get())
+        conf_m_pwd = str(self.conf_pass_field.get())
+        m_pwd = db.return_master_password()
+        if curr_m_pwd == m_pwd:
+            if conf_m_pwd == new_m_pwd:
+                if new_m_pwd != curr_m_pwd:
+                    res = db.update_master_pwd(new_m_pwd)
+                    if res:
+                        messagebox.showinfo("Change master password", "Master password changed successfully !")
+                        self.chng_master_pwd_win.destroy()
+                    else:
+                        messagebox.showwarning("Change master password", "There was a problem changing the password.")
+                        self.chng_master_pwd_win.wait_window()
+                else:
+                    messagebox.showwarning("Change master password", "The old and the new password cannot be same. Please enter a unique password")
+                    self.chng_master_pwd_win.wait_window()
+            else:
+                messagebox.showwarning("Change master password", "The confirmed password doesnt match the new password. Please check")
+                self.chng_master_pwd_win.wait_window()
+        else:
+            messagebox.showwarning("Change master password", "Please enter the current master password correctly\n")
+            self.chng_master_pwd_win.wait_window()
+
+
+    def logout(self):
+        ans = messagebox.askyesnocancel('Logout', 'Are you sure you want to log out of the vault')
+        if ans == 'yes':
+            # Insert WM_PROTOCOL to destroy all windows
+            self.pass_table.destroy()
+            messagebox.showinfo('Logout', 'Goodbye! Have a nice day ahead')
+        else:
+            self.pass_table.wait_window()
+
+
+
+
+
+
+
+
+
+
+
+# BARE BONES IMPLEMENATION OF THE PASSWORD MANAGER USER INTERFACE
 close = False
 
 def manager():
@@ -50,7 +312,7 @@ def create_password_vault():
     menu()
 
 
-def login_password_vault():   # TODO: No input argument m_user
+def login_password_vault():
     m_pwd = db.return_master_password()
     pass_key = str(input("Enter the master passkey: "))
     attempt = 0
@@ -98,7 +360,7 @@ def menu():
         print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 
         try:
-            sel = int(input("Enter your selection (1-7): "))
+            sel = int(input("Enter your selection (1-8): "))
         except ValueError:
             print("Invalid Choice!. Please enter a number in the range (1-7)")
             continue
@@ -107,7 +369,7 @@ def menu():
             web = str(input("Enter the website: "))
             username = str(input("Enter the username. Use '_' instead of whitespace: "))
             password = str(input("Enter the password: "))
-            table_exists = db.is_table_present()
+            table_exists = db.is_password_table()
             if table_exists:
                 sl_no = db.return_serial_number()
             else:
@@ -119,9 +381,9 @@ def menu():
         elif sel == 2:
             values = db.print_all_values()
             if len(values) == 0:
-                print("The vault is empty. Start adding some passwords\n")
+                print("The vault is empty. Start adding some passwords")
             else:
-                print("The entries in the database are as follows: ")
+                print("\nThe entries in the database are as follows: ")
                 print_entries(values)
             print('\n')
 
@@ -152,9 +414,10 @@ def menu():
             print('\n')
 
         elif sel == 6:
-            cols = list(input("Enter the columns you wish to update. Use ', ' as separator: ").split(', '))
-            vals = list(input("Enter the new credentials in the same order as the column names. Use ', ' as separator: ").split(', '))
-            res = db.update_credentials(cols, vals)
+            num = str(input("Enter the sl_no of the entry you wish to update: "))
+            col = str(input("Enter the name of the column you wish to update: "))
+            val = str(input("Enter the new credentials for the above mentioned column: "))
+            res = db.update_credentials(num, col, val)
             if res:
                 print("The credentials have been successfully updated")
             else:
@@ -162,25 +425,29 @@ def menu():
 
         elif sel == 7:
             while True:
-                old_m_pwd = str(input("Enter the old master password: "))
+                curr_m_pwd = str(input("Enter the current master password: "))
                 new_m_pwd = str(input("Enter the new master password: "))
                 conf_m_pwd = str(input("Confirm the new master password: "))
-                if conf_m_pwd == new_m_pwd:
-                    if new_m_pwd != old_m_pwd:
-                        res = db.update_master_pwd(new_m_pwd)
-                        if res:
-                            print("Password changed successfully !")
-                            break
+                m_pwd = db.return_master_password()
+                if curr_m_pwd == m_pwd:
+                    if conf_m_pwd == new_m_pwd:
+                        if new_m_pwd != curr_m_pwd:
+                            res = db.update_master_pwd(new_m_pwd)
+                            if res:
+                                print("Master password changed successfully !\n")
+                                break
+                            else:
+                                print("There was a problem changing the password.\n")
                         else:
-                            print("There was a problem changing the password.")
+                            print("The old and the new password cannot be same. Please enter a unique password\n")
                     else:
-                        print("The new password and the old password cannot be same")
+                        print("The confirmed password doesnt match the new password. Please check\n")
                 else:
-                    print("The new password doesnt match the confirm password. Please check")
+                    print("Please enter the current master password correctly\n")
 
     print("You have successfully logged out of the vault.")
     close = True
 
 
 if __name__ == '__main__':
-    manager()
+    ui = UserInterface()
