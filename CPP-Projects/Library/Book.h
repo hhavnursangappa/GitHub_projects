8fb00115c1b69f1853c94cf3138290800dfe14e3
h-havnursangappa <h.havnursangappa@gmail.com>
2021-02-22 20:30:11 +0100

    No changes

--- CPP-Projects/Library/Book.h
+++ CPP-Projects/Library/Book.h
@@ -1,5 +1,5 @@
-#ifndef BOOK
-#define BOOK
+#ifndef BOOK_H // Avoids inclusind this header file if it is already included
+#define BOOK_H
 #include <iostream>
 #include <string>
 
@@ -11,11 +11,19 @@ public:
 	int Id;
 	std::string Title;
 	std::string Author;
-	bool CheckedOut = false;
+	bool CheckedOut;
 	//---------------------------------------------------------------------------------------------------------------------------
-	Book();
-	Book(int id, std::string title, std::string author);
+	Book(); // Declaration of the default constructor for the class 'Book'
+	Book(int id, std::string title, std::string author); // Declaration of the custom constructor for the class 'Book'
 	//---------------------------------------------------------------------------------------------------------------------------
+	/* Syntax for operator overloading:
+	<return_data_type> operator <operand>(const <name of the user defined class> &input_argument)
+	{
+		code block;
+	}
+
+	If the overloading is done outside the class then specify 2 operands as input arguments */
+	
 	bool operator==(const Book &book)
 	{
 		if (Title.compare(book.Title) == 0)
