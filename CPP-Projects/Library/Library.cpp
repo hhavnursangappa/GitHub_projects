329b6b6ff865ec7ce3bcd39631af2f62029145a7
h-havnursangappa <h.havnursangappa@gmail.com>
2021-02-22 09:23:29 +0100

    Updated the Library file

--- CPP-Projects/Library/Library.cpp
+++ CPP-Projects/Library/Library.cpp
@@ -1,119 +1,117 @@
-// Library.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
+#include <iostream>
+#include <string>
 #include "Book.h"
 #include "Inventory.h"
-#include <string>
 
 using namespace std;
 
-Inventory _inventory;  // Declare a global class object for an inventory '_' to denote it is a global variable
+Inventory _inventory; // underscore to denote that it is a global variable
 
 int main()
 {
 	while (true)
 	{
-		cout << "Choose an option: " << endl;
-		cout << "1. Add Book " << endl;
-		cout << "2. List all Books" << endl;
-		cout << "3. Check out book" << endl;
-		cout << "4. Check in book" << endl;
-		cout << "0. Exit" << endl;
-		
-		int input;
-		cin >> input;
-		cin.ignore();  // When 'cin' and 'getline' are used together, it may cause some problems. So adding this will resolve it
+		int choice;
 
-		switch (input)
+		cout << "\nChoose an option: \n";
+		cout << "1 = Add Book\n";
+		cout << "2 = List all Books\n";
+		cout << "3 = Checkout a Book\n";
+		cout << "4 = Checkin a Book\n";
+		cout << "0 = Exit\n";
+
+		cin >> choice;
+		cin.ignore(); // When 'cin' and 'getline' are used together, it may cause some problems. So adding this will resolve it
+
+		switch (choice)
 		{
 			case 0:
-				cout << "Thank you. Goodbye" << endl;
+			{
+				cout << "Thank you. Goodbye !";
 				return 0;
+			}
 
 			case 1:
 			{
+				string title, author;
+				int id;
+
 				cout << "Title: ";
-				string title;
-				getline(cin, title);
-
 				cout << "Author: ";
-				string author;
-				getline(cin, author);
 
-				int id = _inventory.Books.size() + 1;
+				getline(cin, title);
+				getline(cin, author);
+				id = _inventory.Books.size() + 1;
 
 				Book newBook(id, title, author);
 				_inventory.AddBook(newBook);
+
+				cout << "The book has been successfully added\n";
 				break;
 			}
 
 			case 2:
-				cout << "ID\tTitle\tAuthor" << endl;
-				for (int ii = 0; ii < _inventory.Books.size(); ii++)
+			{
+				int ii;
+				cout << "ID" << "\t" << "TITLE" << "\t" << "AUTHOR" << endl;
+				for (int ii=0; ii < _inventory.Books.size(); ii++);
 				{
 					cout << _inventory.Books[ii].Id << "\t" << _inventory.Books[ii].Title << "\t" << _inventory.Books[ii].Author << endl;
-				}	
-				cout << endl;
+				}
+
 				break;
+			}
 
 			case 3:
 			{
-				cout << "Enter the title of a book to check out: ";
 				string title;
+				bool res;
+
+				cout << ("Enter the title of the book to checkout: ");
 				getline(cin, title);
 				Book foundBook;
-				if (_inventory.FindBookByTitle(title, foundBook))
+
+				res = _inventory.FindBookByTitle(title, foundBook);
+				if (res)
 				{
 					if (foundBook.CheckedOut)
 					{
-						cout << "Book alreday checked out !\n";
-						break;
-					}
-
-					_inventory.CheckOutBook(foundBook);
-					cout << "Book checked out!\n" << endl;
-				}
-				else
-				{
-					cout << "Book not found\n" << endl;
-				}
-				break;
-			}
-				
-
-			case 4:
-			{
-				cout << "Enter the title of a book to check in: ";
-				string title;
-				getline(cin, title);
-				Book foundBook;
-				if (_inventory.FindBookByTitle(title, foundBook))
-				{
-					if (!foundBook.CheckedOut)
-					{
-						cout << "Book alreday checked in !\n";
-						break;
+						cout << "Book has already been checked out !\n";
 					}
 					else
 					{
-						_inventory.CheckInBook(foundBook);
-						cout << "Book checked in!\n" << endl;
+						_inventory.CheckOutBook(foundBook);
+						cout << "Book is successfully checked out !\n";
 					}
-					
-				}
-				else
-				{
-					cout << "Book not found\n" << endl;
 				}
 				break;
-			}	
+			}
 
-			default:
-				cout << "Invalid Selection. Try again" << endl;
+			case 4:
+			{
+				string title;
+				bool res;
+
+				cout << ("Enter the title of the book to checkin: ");
+				getline(cin, title);
+				Book foundBook;
+
+				res = _inventory.FindBookByTitle(title, foundBook);
+				if (!res)
+				{
+					if (!foundBook.CheckedOut)
+					{
+						cout << "Book already checked in\n";
+					}
+					
+					else
+					{
+						_inventory.CheckInBook(foundBook);
+						cout << "Book successfully checked in!\n";						
+					}
+				}
+				break;								
+			}
 		}
-
 	}
-	return 0;
 }
-
