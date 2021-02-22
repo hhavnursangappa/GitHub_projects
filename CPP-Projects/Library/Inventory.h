75ee2b55c811f075c635510bdfd4ad6b0d68c6cc
h-havnursangappa <h.havnursangappa@gmail.com>
2021-02-22 09:22:48 +0100

    Updated the header file

--- CPP-Projects/Library/Inventory.h
+++ CPP-Projects/Library/Inventory.h
@@ -1,17 +1,16 @@
-#pragma once
-
-#include "Book.h"
+#include <iostream>
+#include <string>
 #include <vector>
+#include "Book.h"
 
 class Inventory
 {
 public:
 	std::vector<Book> Books;
-	
+
 	void AddBook(Book book);
 	void RemoveBook(Book book);
 	bool FindBookByTitle(std::string title, Book &book);
 	void CheckOutBook(Book &book);
 	void CheckInBook(Book &book);
 };
-
