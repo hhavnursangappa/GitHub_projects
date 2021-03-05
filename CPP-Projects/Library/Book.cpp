60d14ec3bba35ba65ad9913135e7cd0ef2fc5fcf
h-havnursangappa <h.havnursangappa@gmail.com>
2021-03-05 07:55:17 +0100

    Added the definition of the methods used to access the private attributes in the Book class

--- CPP-Projects/Library/Book.cpp
+++ CPP-Projects/Library/Book.cpp
@@ -5,15 +5,33 @@
 using namespace std;
 
 // Defining the Default constructor
-Book::Book()
-{
-}
-
+Book::Book(){}
+//-------------------------------------------------------------------------------------------------------
 // Defining the custom constructor
-Book::Book(int id, string title, string author)
+Book::Book(string title, string author)
 {
-	Id = id;
 	Title = title;
 	Author = author;
 	CheckedOut = false;
 }
+//-------------------------------------------------------------------------------------------------------
+void Book::CheckInOrOut(bool checkOut)
+{
+	CheckedOut = checkOut;
+}
+//-------------------------------------------------------------------------------------------------------
+void Book::DisplayBook()
+{
+	cout << Id << "\t" << Title << "\t" << Author << endl;
+}
+//-------------------------------------------------------------------------------------------------------
+bool Book::IsCheckedOut()
+{
+	return CheckedOut;
+}
+//-------------------------------------------------------------------------------------------------------
+void Book::SetBookId(int id)
+{
+	Id = id;
+}
+//-------------------------------------------------------------------------------------------------------
