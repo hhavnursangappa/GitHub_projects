55a6ddc90bc102c8db96ea814aadb896e5a0bebe
h-havnursangappa <h.havnursangappa@gmail.com>
2021-03-05 07:58:03 +0100

    Defined the new methods declared in Inventory.h

--- CPP-Projects/Library/Inventory.cpp
+++ CPP-Projects/Library/Inventory.cpp
@@ -5,40 +5,104 @@
 #include "Book.h"
 #include <algorithm>
 
+using namespace std;
+
+Inventory::Inventory()
+{
+	Inventory::MaxBookId = 0;
+}
+//-------------------------------------------------------------------------------------------------------
+
 void Inventory::AddBook(Book book)
 {
+	Inventory::MaxBookId++;
+	book.SetBookId(MaxBookId);
 	Inventory::Books.push_back(book);
 }
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+
+Book Inventory::GetBookByIndex(int index)
+{
+	return Inventory::Books[index];
+}
+//-------------------------------------------------------------------------------------------------------
+
+int Inventory::NumberOfBooks()
+{
+	return Inventory::Books.size();
+}
+//-------------------------------------------------------------------------------------------------------
+
 void Inventory::RemoveBook(std::string title)
 {
-	std::vector<Book>::iterator pos = std::find(Inventory::Books.begin(), Inventory::Books.end(), Book(0, title, ""));
+	//TODO: Handle MaxBookId when the entry with the MaxBookId is deleted
+
+	std::vector<Book>::iterator pos = std::find(Inventory::Books.begin(), Inventory::Books.end(), Book(title, ""));
 	if (pos != Inventory::Books.end())
 	{
 		Inventory::Books.erase(pos);
 	}
 }
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
+
 int Inventory::FindBookByTitle(std::string title)
 {
 	/* This is an iterator in C++. The begin() method points to the first element 
 	of the vector and the end() method pints to the last element of the vector. This means iterate through the vector from first to last and find the object Book(0, title, ""). This avoids the use 
 	of another for loop for the same purpose */
-	std::vector<Book>::iterator it = std::find(Inventory::Books.begin(), Inventory::Books.end(), Book(0, title, "")); // str::find returns the position of the element
-	if (it == Inventory::Books.end())																				  // == Books.end() means the element was not found
+
+	std::vector<Book>::iterator it = std::find(Inventory::Books.begin(), Inventory::Books.end(), Book(title, "")); // str::find returns the position of the element
+
+	if (it == Inventory::Books.end()) // "== Books.end()" means the element was not found
 	{
 		return -1;
 	}
-	int index = it - Inventory::Books.begin(); // Try without Books.begin()
+	int index = it - Inventory::Books.begin();
 	return index;
 }
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-void Inventory::CheckOutBook(Book *book)
+//-------------------------------------------------------------------------------------------------------
+
+CheckInOrOutResult Inventory::CheckInOrOutBook(std::string title, bool checkOut)
 {
-	book->CheckedOut = true; // '->' has to be used to acces the properties of the pointer
+	int foundBookIndex = FindBookByTitle(title);
+
+	if (foundBookIndex < 0)
+	{
+		return CheckInOrOutResult::BookNotFound;
+	}
+
+	Books[foundBookIndex].CheckInOrOut(checkOut);
+	return CheckInOrOutResult::Success;
 }
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-void Inventory::CheckInBook(Book *book)
+//-------------------------------------------------------------------------------------------------------
+
+void Inventory::DisplayAllBooks()
 {
-	book->CheckedOut = false; // '->' has to be used to acces the properties of the pointer
+	cout << "ID"
+		 << "\t"
+		 << "TITLE"
+		 << "\t"
+		 << "AUTHOR" << endl;
+	for (int ii = 0; ii < NumberOfBooks(); ii++)
+	{
+		Books[ii].DisplayBook();
+	}
 }
+//-------------------------------------------------------------------------------------------------------
+
+void Inventory::DisplayCheckedOutBooks()
+{
+	cout << "ID"
+		 << "\t"
+		 << "TITLE"
+		 << "\t"
+		 << "AUTHOR" << endl;
+	for (int ii = 0; ii < NumberOfBooks(); ii++)
+	{
+		if (GetBookByIndex(ii).IsCheckedOut())
+		{
+			Books[ii].DisplayBook();
+		}
+	}
+}
+//-------------------------------------------------------------------------------------------------------
