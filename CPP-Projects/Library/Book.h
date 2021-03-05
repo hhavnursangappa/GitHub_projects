083c710c98da250bd6536d7126dfd8d9ff4b09c3
h-havnursangappa <h.havnursangappa@gmail.com>
2021-03-05 07:54:29 +0100

    Made all attributes private and aded methods to access these attributes

--- CPP-Projects/Library/Book.h
+++ CPP-Projects/Library/Book.h
@@ -7,29 +7,35 @@
 
 class Book
 {
-public:
-	int Id;
-	std::string Title;
-	std::string Author;
-	bool CheckedOut;
-	//---------------------------------------------------------------------------------------------------------------------------
-	Book(); // Declaration of the default constructor for the class 'Book'
-	Book(int id, std::string title, std::string author); // Declaration of the custom constructor for the class 'Book'
-	//---------------------------------------------------------------------------------------------------------------------------
-	/* Syntax for operator overloading:
-	<return_data_type> operator <operand>(const <name of the user defined class> &input_argument)
-	{
-		code block;
-	}
+	private:
+		int Id;
+		std::string Title;
+		std::string Author;
+		bool CheckedOut;
+//-------------------------------------------------------------------------------------------------------
+	public:
+		Book();										 // Declaration of the default constructor for the class 'Book'
+		Book(std::string title, std::string author); // Declaration of the custom constructor for the class 'Book'
+		void CheckInOrOut(bool checkOut);
+		void DisplayBook();
+		bool IsCheckedOut();
+		void SetBookId(int id);
+		//-----------------------------------------------------------------------------------------------
+		/* Syntax for operator overloading:
+			<return_data_type> operator <operand>(const <name of the user defined class> &input_argument)
+			{
+				code block;
+			}
 
-	If the overloading is done outside the class then specify 2 operands as input arguments */
-	
-	bool operator==(const Book &book)
-	{
-		if (Title.compare(book.Title) == 0)
-			return true;
-		else
-			return false;
-	}
+			If the overloading is done outside the class then specify 2 operands as input arguments */
+
+		bool operator==(const Book &book) const
+		{
+			if (Title.compare(book.Title) == 0)
+				return true;
+			else
+				return false;
+		}
+//------------------------------------------------------------------------------------------------------
 };
 #endif
