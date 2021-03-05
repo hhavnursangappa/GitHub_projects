ae3ff52d91e575ff6e12f26d4dca115f7a8ce597
h-havnursangappa <h.havnursangappa@gmail.com>
2021-03-05 07:56:59 +0100

    Eliminated unnecessary methods and  declared new methods

--- CPP-Projects/Library/Inventory.h
+++ CPP-Projects/Library/Inventory.h
@@ -2,15 +2,24 @@
 #include <string>
 #include <vector>
 #include "Book.h"
+#include "CheckInOrOutResult.h"
 
 class Inventory
 {
-public:
+private:
+	int MaxBookId;
 	std::vector<Book> Books;
 
+public:
+	Inventory();
+
+	//int GetNextBookId();  // Avoided this function as it may be abused by users to simply incerment the Id without adding books
+	int NumberOfBooks();
+	Book GetBookByIndex(int index);
 	void AddBook(Book book);
 	void RemoveBook(std::string title);
 	int FindBookByTitle(std::string title);
-	void CheckOutBook(Book *book);
-	void CheckInBook(Book *book);
+	CheckInOrOutResult CheckInOrOutBook(std::string title, bool checkOut);
+	void DisplayAllBooks();
+	void DisplayCheckedOutBooks();
 };
